1c6b078fbb9d729feb1871a28d0a9889f48b78f4
Tao <werewaswas@live.com>
2018-06-19 22:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 let totalBatteries;
 
-const batteryCallback = (a,b) => return (a + b)
+const batteryCallback = (a,b) => return (a + b);
 
 totalBatteries = batteryBatches.reduce(batteryCallback, 0);
 
