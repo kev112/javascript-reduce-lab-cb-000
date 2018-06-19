8769347f33d7b49c08d4327dc2be14639d2acacc
Tao <werewaswas@live.com>
2018-06-19 21:17:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 let totalBatteries;
 
-totalBatteries = batteryBatches.reduce((a,b) => { a + b }, 0);
+totalBatteries = batteryBatches.reduce((a,b) =>  a + b , 0);
 
 let wordCountMap;
 
